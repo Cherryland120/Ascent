352f8eb87d373696b760c0aff697d16b905dcb0d
Anointing Tamunowunari-Tasker <anointingtasker2002@gmail.com>
2026-07-30 14:31:58 +0100

    cli calculator
    Made a cli basic calculator that deals with only two inputs and an operator.

---
+++ cli-calculator/src/main.rs
@@ -0,0 +1,49 @@
+use std::io::{self, Write};
+#[derive(Debug)]
+
+struct Operation{
+    left: i32,
+    right: i32,
+    operator: char,
+}
+
+fn create_operation()-> Operation{
+    Operation{left: 0, right: 0, operator: '+'}
+}
+
+fn main() {
+    println!("Welcome to RustyBeer!");
+    let mut input = String::new();
+    let mut operation = create_operation();
+
+    println!("Enter first input:");
+    io::stdin().read_line(&mut input).expect("Failed to read the line");
+    operation.left = input.trim().parse::<i32>().expect("That's not a number");
+    io::stdout().flush().unwrap();
+    input.clear();
+
+    println!("Enter operator: ");
+    io::stdin().read_line(&mut input).expect("Failed to read the line");
+    operation.operator = input.trim().parse::<char>().expect("That's not a character");
+    io::stdout().flush().unwrap();
+    input.clear();
+
+    println!("Enter second input: ");
+    io::stdin().read_line(&mut input).expect("Failed to read the line");
+    operation.right = input.trim().parse::<i32>().expect("That's not a number");
+    input.clear();
+
+    if operation.operator == '+'{
+        println!("The answer is: {}", operation.left + operation.right);
+    }
+    else if operation.operator == '-'{
+        println!("The answer is: {}", operation.left - operation.right);
+    }
+    else if operation.operator == '*'{
+        println!("The answer is: {}", operation.left * operation.right);
+    }
+    else if operation.operator == '/'{
+        println!("The answer is: {}", operation.left / operation.right);
+    }
+
+}
